1224bef0fc941f4c4f59c8edb955fe01a944b1bd
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-10 10:29:35 +0100

    q

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const key = '32820375-2ad81e3b731d859a46f31c661';
 
 export default async function getImages(q, page) {
     try {
-        const filter = `?key=${key}&q${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`;
+        const filter = `?key=${key}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`;
         const response = await axios.get(filter)
         return response.data;
     } catch (error) {
